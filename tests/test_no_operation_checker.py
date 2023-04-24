f5a46fb5f110d8c0e231ae2f471261b04cb94023
nekanat <nekanat.stock@gmail.com>
2023-04-24 21:38:31 +0900

    exclude abc

--- tests/test_no_operation_checker.py
+++ tests/test_no_operation_checker.py
@@ -5,6 +5,17 @@ from mle_flake8_plugins.flake8_mle_convention.errors import error_codes
 
 def test_use_no_operations():
     code = """
+global_var = 0
+
+def test():
+    return global_var
+"""
+    tree = ast.parse(code, '')
+    visitor = NoOperationsChecker()
+    visitor.visit(tree)
+    assert len(visitor.errors) == 1
+
+    code = """
 def test(a, b):
     pass
 """
@@ -25,6 +36,41 @@ def test(a, b):
     code = """
 def test(a, b):
     return a + b
+"""
+    tree = ast.parse(code, '')
+    visitor = NoOperationsChecker()
+    visitor.visit(tree)
+    assert len(visitor.errors) == 0
+
+    code = """
+class Test:
+    def test(self, a, b):
+        return a
+"""
+    tree = ast.parse(code, '')
+    visitor = NoOperationsChecker()
+    visitor.visit(tree)
+    assert len(visitor.errors) == 1
+
+    code = """
+class Test:
+    a = 0
+
+    @property
+    def test(self):
+        return self.a
+"""
+    tree = ast.parse(code, '')
+    visitor = NoOperationsChecker()
+    visitor.visit(tree)
+    assert len(visitor.errors) == 0
+
+
+    code = """
+import abc
+class Test(abc.ABC):
+    def test(self, a):
+        return a
 """
     tree = ast.parse(code, '')
     visitor = NoOperationsChecker()
